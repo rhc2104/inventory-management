d256cbd481479e37734edc079d467164322574ef
Ronnie Cheng <ronald.cheng@fractional.ai>
2026-07-27 11:04:46 -0700

    Add design token layer and import it at app entry

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import './styles/tokens.css'
 import App from './App.vue'
 import Dashboard from './views/Dashboard.vue'
 import Inventory from './views/Inventory.vue'
